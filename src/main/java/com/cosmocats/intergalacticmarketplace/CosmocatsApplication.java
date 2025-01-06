216f2195a053bc856d03687edd2b11efa30d30c5
zlevskaolena <zalevskaalena25@gmail.com>
2025-01-06 16:25:17 +0200

    initial

---
+++ src/main/java/com/cosmocats/intergalacticmarketplace/CosmocatsApplication.java
@@ -0,0 +1,13 @@
+package com.cosmocats.intergalacticmarketplace;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CosmocatsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CosmocatsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/cosmocats/intergalacticmarketplace/CosmocatsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cosmocats.intergalacticmarketplace;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CosmocatsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
